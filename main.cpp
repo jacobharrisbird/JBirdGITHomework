233385406771033945c772ae3ab0a8923f894b44
Jacob Harris Bird <apple@Apple-MacBook-Pro-15-inch-Early.local>
2016-10-14 11:26:19 -0700

    note

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GIT
+//
+//  Created by Jacob Harris Bird on 10/14/16.
+//  Copyright © 2016 Cogswell Polytechnical College. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "New Line\n";
+    std::cout << "Hello, World!\n";
+    return 0;
+}
